fff08be2030bbc9182f39e12cd06d153632d109d
Jyneda <jyneda@gmail.com>
2018-12-10 22:05:48 +0800

    Add files via upload

---
+++ thread_example.py
@@ -0,0 +1,23 @@
+import queue
+import threading
+import random
+import time
+
+# called by each thread
+def f (arg):
+    w = random.randint(0, 5)
+    time.sleep(w)
+    q.put( (arg, w) )
+
+q = queue.Queue()
+
+for i in range(10):
+    t = threading.Thread(target=f, args=(i,))
+    t.daemon = True     # this ensures thread ends when main process ends
+    t.start()
+
+for i in range(10):
+    s = q.get()
+    print(s)
+
+time.sleep(10000)
