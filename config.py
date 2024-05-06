bb5cafc10d5ebee71bd5c3c3989c55cf005595f3
Jyneda <jyneda@gmail.com>
2024-05-06 22:21:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+### LineBot Information ###
+ChannelAccessToken = 'Please provide ChannelAccessToken'
+ChannelSecret = 'Please provide ChannelSecret'
+
+### Line ID File Path ###
+idfilePath = './userIdFile'
+
---
+++ example.py
@@ -0,0 +1,17 @@
+import time, threading
+import LineBot
+
+def ShowMsg():
+    while True:    
+        if len(LineBot.msg_queue) >0:
+            msg = LineBot.msg_queue.pop(0)
+            print(f'LineBot message = {msg}')
+        time.sleep(5)
+
+t = threading.Thread(target=ShowMsg)
+t.daemon = True   
+t.start()        
+        
+LineBot.init(port=32768)
+
+
